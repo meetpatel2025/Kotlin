9dc09891bf982e9ae49865c652030d27371267b4
Meet Patel <meet.patel@einfochips.com>
2026-02-16 17:49:42 +0530

    Create app using jetpack compose UI and maintain the state

---
+++ JetpackBasics/app/src/main/java/com/training/jetpackbasics/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.training.jetpackbasics
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.training.jetpackbasics.ui.theme.JetpackBasicsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            NotificationScreen()
+        }
+    }
+}
+
+//@Composable
+//fun Greeting(name: String, modifier: Modifier = Modifier) {
+//    Text(
+//        text = "Hello $name!",
+//        modifier = modifier
+//    )
+//}
+//
+//@Preview(showBackground = true)
+//@Composable
+//fun GreetingPreview() {
+//    JetpackBasicsTheme {
+//        Greeting("Android")
+//    }
+//}
---
+++ JetpackBasics/app/src/main/java/com/training/jetpackbasics/StateExample.kt
@@ -0,0 +1,39 @@
+package com.training.jetpackbasics
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Preview
+@Composable
+fun NotificationScreen(){
+    var count: MutableState<Int> = rememberSaveable { mutableIntStateOf(0) }
+    Column(verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize(1f)) {
+        NotificationCounter(count.value, {count.value++}, Modifier.padding(12.dp))
+
+    }
+}
+
+@Composable
+fun NotificationCounter(count:Int, increment:() -> Unit, modifier: Modifier){
+    Column(verticalArrangement = Arrangement.Center) {
+        Text(text = "You have sent $count notifications")
+        Button(onClick = {increment()}) {
+            Text(text = "Send Notification")
+        }
+    }
+}
